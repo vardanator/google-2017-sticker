a926bcb292d7d341f0d8254903681a186c34c2e9
vardanator <vardan.l.grigoryan@gmail.com>
2018-02-01 02:52:56 +0400

    remove junk

--- 109673.java
+++
@@ -1,7 +0,0 @@
-class Mlass {
-
-    public static void main(String[] args) {
-        System.out.println("yeaaah, hiii");
-    }
-
-}
--- b.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() 
-{
-    int a;
-    std::cout << "fooling codetrace";
-}
